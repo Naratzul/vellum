074f4a8ddc4dff8d633e2749c744b24c4183c7fd
Naratzul <naratzul@gmail.com>
2026-06-25 22:00:54 +0300

    add vscode ext workflow; update compiler workflow

--- libs/vellum-diag/vellum_sentry.cmake
+++ libs/vellum-diag/vellum_sentry.cmake
@@ -1,18 +1,28 @@
 # Links crash reporting (vellum_diag) into an app target and sets Sentry compile definitions.
 #
 # Usage (after add_subdirectory(libs/vellum-diag)):
-#   vellum_target_sentry(my_app "my-app@${PROJECT_VERSION}")
+#   vellum_target_sentry(vellum "vellum@${PROJECT_VERSION}" DSN VELLUM_SENTRY_DSN)
+#   vellum_target_sentry(vellum-lsp "vellum-lsp@${PROJECT_VERSION}" DSN VELLUM_LSP_SENTRY_DSN)
 function(vellum_target_sentry target release)
   if(NOT TARGET vellum_diag)
     message(FATAL_ERROR "vellum_target_sentry: vellum_diag must be defined first")
   endif()
 
+  cmake_parse_arguments(SENTRY "" "DSN" "" ${ARGN})
+
+  set(_dsn "")
+  if(SENTRY_DSN AND DEFINED ${SENTRY_DSN} AND NOT "${${SENTRY_DSN}}" STREQUAL "")
+    set(_dsn "${${SENTRY_DSN}}")
+  elseif(NOT SENTRY_DSN AND DEFINED VELLUM_SENTRY_DSN AND NOT VELLUM_SENTRY_DSN STREQUAL "")
+    set(_dsn "${VELLUM_SENTRY_DSN}")
+  endif()
+
   target_link_libraries(${target} PRIVATE vellum_diag)
   target_compile_definitions(${target} PRIVATE
     "VELLUM_SENTRY_RELEASE=\"${release}\"")
-  if(DEFINED VELLUM_SENTRY_DSN AND NOT VELLUM_SENTRY_DSN STREQUAL "")
+  if(NOT _dsn STREQUAL "")
     target_compile_definitions(${target} PRIVATE
-      "VELLUM_SENTRY_DSN=\"${VELLUM_SENTRY_DSN}\"")
+      "VELLUM_SENTRY_DSN=\"${_dsn}\"")
   endif()
   if(DEFINED VELLUM_SENTRY_ENVIRONMENT AND NOT VELLUM_SENTRY_ENVIRONMENT STREQUAL "")
     target_compile_definitions(${target} PRIVATE
