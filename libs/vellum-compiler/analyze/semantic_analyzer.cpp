f1449cbebcb52807cb78b435430f526df2c83ff0
Naratzul <naratzul@gmail.com>
2026-06-26 19:00:50 +0300

    cast from fun error message

--- libs/vellum-compiler/analyze/semantic_analyzer.cpp
+++ libs/vellum-compiler/analyze/semantic_analyzer.cpp
@@ -1022,8 +1022,10 @@ void SemanticAnalyzer::visitUnaryExpression(ast::UnaryExpression& expr) {
 }
 
 void SemanticAnalyzer::visitCastExpression(ast::CastExpression& expr) {
-  expr.getExpression()->accept(*this);
-  if (!expr.getExpression()->hasResolvedType()) {
+  auto& innerExpr = expr.getExpression();
+  innerExpr->accept(*this);
+
+  if (!innerExpr->hasResolvedType()) {
     return;
   }
 
@@ -1039,7 +1041,7 @@ void SemanticAnalyzer::visitCastExpression(ast::CastExpression& expr) {
   target.setType(targetType);
   target.setIdentifierType(VellumValueType::ScriptType);
 
-  const VellumType innerType = expr.getExpression()->getType();
+  const VellumType innerType = innerExpr->getType();
 
   if (!checker.canExplicitlyCast(innerType, targetType)) {
     if (targetType.isArray()) {
@@ -1048,10 +1050,32 @@ void SemanticAnalyzer::visitCastExpression(ast::CastExpression& expr) {
           "Cannot cast to array type. (Skyrim: nothing can be "
           "cast to an array, including other arrays.)");
     } else {
-      errorHandler->errorAt(target.getLocation(),
-                            CompilerErrorKind::InvalidCast,
-                            "Cannot cast from '{}' to '{}'.",
-                            innerType.toString(), targetType.toString());
+      bool srcIsFunction =
+          innerExpr->isIdentifierExpression()
+              ? innerExpr->asIdentifier().getIdentifierType() ==
+                    VellumValueType::Function
+              : false;
+
+      bool srcIsMethod = innerExpr->isPropertyGetExpression()
+                             ? innerExpr->asPropertyGet().getIdentifierType() ==
+                                   VellumValueType::Function
+                             : false;
+
+      if (srcIsFunction || srcIsMethod) {
+        VellumIdentifier function =
+            srcIsFunction ? innerExpr->asIdentifier().getIdentifier()
+                          : innerExpr->asPropertyGet().getProperty();
+
+        errorHandler->errorAt(
+            innerExpr->getLocation(), CompilerErrorKind::InvalidCast,
+            "Cannot cast from function '{}'. Did you mean to call it?",
+            function.toString());
+      } else {
+        errorHandler->errorAt(target.getLocation(),
+                              CompilerErrorKind::InvalidCast,
+                              "Cannot cast from '{}' to '{}'.",
+                              innerType.toString(), targetType.toString());
+      }
     }
     return;
   }
